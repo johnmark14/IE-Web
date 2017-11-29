acffa477ad46a0d9c6a74a8a4c9c52a33403dbd7
jm <johmarkgabrentina@gmail.com>
2017-11-29 17:00:43 +0800

    Fix logout flashing you are not login

--- dashboard.js
+++ dashboard.js
@@ -1,28 +1,29 @@
 document.getElementById("logout").onclick = function() {
     if (firebase.auth().currentUser) {
-        // [START signout]
-        firebase.auth().signOut();
 
+        firebase.auth().signOut();
         var url = "index.html";
         window.open(url, '_top');
+        throw new Error('');
     }
 };
 
-
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        // ...
-    } else {
-        window.alert("You are not signin!");
-        var url = "index.html";
-        window.open(url, '_top');
-    }
-});
+function checkIfLogin() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var isAnonymous = user.isAnonymous;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            // ...
+        } else {
+            window.alert("You are not signin!");
+            var url = "index.html";
+            window.open(url, '_top');
+        }
+    });
+}
