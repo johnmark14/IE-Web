75f625bbb41898f97a6899846ac60d72e15dd73f
John Mark <johmarkgabrentina@gmail.com>
2017-12-01 22:37:15 +0800

    fix some bugs in the login and signout form

--- dashboard.js
+++ dashboard.js
@@ -1,29 +1,30 @@
-document.getElementById("logout").onclick = function() {
-    if (firebase.auth().currentUser) {
 
+document.getElementById("logout").onclick = function() {
+    var checkUserLogout = confirm("Press a button!");
+    if (checkUserLogout) {
         firebase.auth().signOut();
-        var url = "index.html";
-        window.open(url, '_top');
-        throw new Error('');
     }
+    
 };
 
-function checkIfLogin() {
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in.
-            var displayName = user.displayName;
-            var email = user.email;
-            var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            var providerData = user.providerData;
-            // ...
-        } else {
-            window.alert("You are not signin!");
-            var url = "index.html";
-            window.open(url, '_top');
-        }
-    });
-}
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+    } else {
+        window.location.href = 'index.html';
+    }
+});
+
+
+
+
+
+
